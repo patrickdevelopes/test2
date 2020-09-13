bbc181bcf5b5da9960526992843e753d89668dd1
dev <dev@Patricks-MacBook-Pro.local>
2020-09-13 16:03:19 +0300

    Added my project

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,7 +90,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         googleAnalytics: {
           trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
