3fe53573090e3bb29e8a594e63dc6cf5823bfbac
dev <dev@Patricks-MacBook-Pro.local>
2020-09-14 11:20:35 +0300

    Added my project

--- src/pages/strains.js
+++ src/pages/strains.js
@@ -2,7 +2,6 @@ import React from "react"
 import { graphql } from "gatsby"
 import { Link } from "gatsby"
 import styled from "styled-components"
-import Layout from "../components/Layout"
 import ProductBanner from "../components/ProductBanner"
 import ShopFront from '../components/ShopFront'
 import { GoSettings } from 'react-icons/go'
@@ -16,13 +15,11 @@ const Strains = ({ data }) => {
 
   return (
     <Wrapper>
-      <Layout>
-        <ShopFront />
-        <section className="posts">
-          <h3 className="posts-title-products"><GoSettings />Browse Seeds</h3>
-          <p className="posts-text-strains">Auto-flowering marijuana seeds | Autoflowering cannabis plants start flowering on their own within 2-4 weeks of germination. Classic varieties, both Indica and Sativa dominant, are photoperiodic plants. Therefore, they are traditionally exposed to 12 hours of light in order to induce flowering indoors but might not have enough time to finish outdoors before the days get shorter – unlike autoflowering strains.They are Indica or Sativa plants crossbred with a Ruderalis representative, making them the perfect choice for areas where summer.</p>
-        </section>
-      </Layout>
+      <ShopFront />
+      <section className="posts">
+        <h3 className="posts-title-products"><GoSettings />Browse Seeds</h3>
+        <p className="posts-text-strains">Auto-flowering marijuana seeds | Autoflowering cannabis plants start flowering on their own within 2-4 weeks of germination. Classic varieties, both Indica and Sativa dominant, are photoperiodic plants. Therefore, they are traditionally exposed to 12 hours of light in order to induce flowering indoors but might not have enough time to finish outdoors before the days get shorter – unlike autoflowering strains.They are Indica or Sativa plants crossbred with a Ruderalis representative, making them the perfect choice for areas where summer.</p>
+      </section>
     </Wrapper>
   )
 }
